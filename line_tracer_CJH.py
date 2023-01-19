a657ad1cfec877d538ac4b88f78224b4dc51d989
최정환 <81159918+jungwhan-choi@users.noreply.github.com>
2023-01-19 09:43:01 +0900

    triangle space securing V1.1
    도로위의 차선만 선으로 인식하기 위한 삼각형 공간 확보 함수 만드는 작업

--- line_tracer_CJH.py
+++ line_tracer_CJH.py
@@ -6,17 +6,26 @@ import matplotlib.pyplot as plt
 def canny(image):
     gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)  # RGB값을 조정해 회색으로 만듦
     blur = cv2.GaussianBlur(gray, (5, 5), 0)  # 5x5 커널을 활용해 가우시안 블러처리
-    canny = cv2.Canny(blur, 50, 150)
+    canny = cv2.Canny(blur, 50, 150)  # canny함수로 변형
     return canny
 
 
+def region_of_interest(image):
+    height = image.shape[0]  # y축 높이가 0이됨을 뜻함
+    triangle = np.array(
+        [(600, height), (1300, height), (850, 300)]
+    )  # 삼각형의 공간을 x,y좌표계를 활용하여 가져온다
+    mask = np.zeros_like(image)
+
+
 while 1:
+
     # _, frame = cam.read()
     image = cv2.imread("road_test.jpg")  # 이미지를 가져옴
-    lane_image = np.copy(image)  # 원본 이미지에 영향을 주지 않기 위해 copy 사용
+    lane_image = cv2.copy(image)  # 원본 이미지에 영향을 주지 않기 위해 copy 사용
     canny = canny(lane_image)
 
-    plt.imshow(canny)  # 이미지 보여줌
-    plt.show()
+    cv2.imshow("result", canny)  # 이미지 보여줌
+
     if cv2.waitKey(1) == ord("q"):
         break  # q키 누르기 전까지 이미지 imshow
