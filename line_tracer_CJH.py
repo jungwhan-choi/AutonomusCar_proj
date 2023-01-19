175da1e575f8fa0dbf7d59a0d59df971d1f87c17
최정환 <81159918+jungwhan-choi@users.noreply.github.com>
2023-01-19 16:06:55 +0900

    interested region func add
    관심부분을 따오는 함수 제작하였음

--- line_tracer_CJH.py
+++ line_tracer_CJH.py
@@ -12,20 +12,26 @@ def canny(image):
 
 def region_of_interest(image):
     height = image.shape[0]  # y축 높이가 0이됨을 뜻함
-    triangle = np.array(
-        [(600, height), (1300, height), (850, 300)]
-    )  # 삼각형의 공간을 x,y좌표계를 활용하여 가져온다
-    mask = np.zeros_like(image)
+    polygons = np.array(  # polygon:3D 객체를 이루는 여러개의 삼각형 입자를 말하는 것
+        [[(600, height), (1300, height), (850, 300)]]
+    )  # 삼각형의 공간을 x,y좌표계를 polygon 중 하나로 임의로 지정한 뒤 가져온다.
+    mask = np.zeros_like(
+        image
+    )  # image의 픽셀 갯수만큼의 zero matrix를 만든다. 이제 이 zero matrix의 공간에 triangle 만큼의 영역을 채운다
+    cv2.fillPoly(
+        mask, polygons, 255
+    )  # fillpoly 함수는 여러개의 polygon에 대해 작동하기 때문에 polygons 변수에 하나의 polygon을 가진 배열이라고 정해놓은 것.
+    return mask  # mask는 그림에서 따오고 싶은 부분만의 영역을 rgb값을 밝게 처리하고 나머지는 0으로 처리한 프레임
 
 
 while 1:
 
     # _, frame = cam.read()
     image = cv2.imread("road_test.jpg")  # 이미지를 가져옴
-    lane_image = cv2.copy(image)  # 원본 이미지에 영향을 주지 않기 위해 copy 사용
+    lane_image = np.copy(image)  # 원본 이미지에 영향을 주지 않기 위해 copy 사용
     canny = canny(lane_image)
 
-    cv2.imshow("result", canny)  # 이미지 보여줌
-
+    cv2.imshow("result", region_of_interest(canny))  # 이미지 보여줌
+    cv2.waitKey(0)
     if cv2.waitKey(1) == ord("q"):
         break  # q키 누르기 전까지 이미지 imshow
