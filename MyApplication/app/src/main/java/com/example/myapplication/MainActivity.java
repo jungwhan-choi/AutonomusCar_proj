56ea7980851cce5c9d73f42ca07cf5e42dc8a0e8
최정환 <81159918+jungwhan-choi@users.noreply.github.com>
2023-01-22 13:27:26 +0900

    chaquopy setting
    chaquopy 추가

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=(TextView)findViewById(R.id.text_view);
+        if(!Python.isStarted()){
+            Python.start(new AndroidPlatform(this));
+        }
+        final Python py=Python.getInstance();
+        PyObject pyobj=py.getModule("script");
+        PyObject obj=pyobj.callAttr("hello");
+        tv.setText(obj.toString());
     }
 }
