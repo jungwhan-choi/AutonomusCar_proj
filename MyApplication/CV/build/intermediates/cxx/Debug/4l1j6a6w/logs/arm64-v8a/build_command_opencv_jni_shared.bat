6d3cb40bfd6a5d7112b9f8e8c4c63d5d81cb4283
최정환 <81159918+jungwhan-choi@users.noreply.github.com>
2023-01-28 15:16:36 +0900

    failed

--- MyApplication/CV/build/intermediates/cxx/Debug/4l1j6a6w/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\zad34\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\zad34\\Documents\\GitHub\\AutonomusCar_proj\\MyApplication\\CV\\.cxx\\Debug\\4l1j6a6w\\arm64-v8a" ^
-  opencv_jni_shared
