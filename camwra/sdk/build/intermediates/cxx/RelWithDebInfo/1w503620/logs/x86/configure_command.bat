84cc43c3936391ced7af8ee4d04baa559640b8a8
최정환 <81159918+jungwhan-choi@users.noreply.github.com>
2023-01-28 16:35:34 +0900

    camera create

---
+++ camwra/sdk/build/intermediates/cxx/RelWithDebInfo/1w503620/logs/x86/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\zad34\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\zad34\\Documents\\GitHub\\AutonomusCar_proj\\camwra\\sdk\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\zad34\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\zad34\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\zad34\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\zad34\\AppData\\Local\\Android\\Sdk\\cmake\\3.18.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\zad34\\Documents\\GitHub\\AutonomusCar_proj\\camwra\\sdk\\build\\intermediates\\cxx\\RelWithDebInfo\\1w503620\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\zad34\\Documents\\GitHub\\AutonomusCar_proj\\camwra\\sdk\\build\\intermediates\\cxx\\RelWithDebInfo\\1w503620\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=RelWithDebInfo" ^
+  "-BC:\\Users\\zad34\\Documents\\GitHub\\AutonomusCar_proj\\camwra\\sdk\\.cxx\\RelWithDebInfo\\1w503620\\x86" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
